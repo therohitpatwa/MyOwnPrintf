9ac79be6c81d4a0e4c4323d2c5e3b8bc32cd2cfe
Rohit Patwa <145226082+therohitpatwa@users.noreply.github.com>
2025-02-01 22:32:15 +0530

    Create testrohitprintf.c

---
+++ testrohitprintf.c
@@ -0,0 +1,33 @@
+#include "rohitprintf.h"
+int	main()
+{
+	int	count;
+
+
+  rohit_printf("Hello this message is printed by rohit printf function\n");
+  
+	count = rohit_printf("Example of String %s\n", "Rohit");
+	printf("%d character\n", count);
+	count = rohit_printf("Example of Char %c\n", 'S');
+	printf("%d character\n", count);
+	count =rohit_printf("Example of Nbr %d\n", -42);
+	printf("%d character\n", count);
+	count = rohit_printf("Example of Hexadecimal %x\n", -1);
+	printf("%d character\n", count);
+
+	//REAL 
+  printf("Hello this message is printed by rohit printf function\n");
+  
+	count = printf("Example of String %s\n", "Rohit");
+	printf("%d character\n", count);
+	count = printf("Example of Char %c\n", 'S');
+	printf("%d character\n", count);
+	count =printf("Example of Nbr %d\n", -42);
+	printf("%d character\n", count);
+	count =printf("Example of Hexadecimal %x\n", -1);
+	printf("%d character\n", count);
+
+  return 0;
+
+
+}

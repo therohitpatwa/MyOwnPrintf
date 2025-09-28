4e6797f3f53f440f16ffdb25aa757c8a9bbbda0c
Rohit Patwa <therohitpatwa@gmail.com>
2025-09-29 01:24:01 +0530

    Implement print_format

--- rohitprintf.h
+++ rohitprintf.h
@@ -4,6 +4,23 @@
 #include <stdio.h>
 
 
+int print_format(char specifier, va_list ap)
+{
+	int count=0;
+	if(specifier=='c')
+	  count+=print_char(va_arg ap, int);
+	else if(specifier=='s')
+	  count+= print_str(va_arg,char *);
+	else if(specifier=='d')
+	  count+=print_digit(va_arg(ap,int),10);
+	else if(specifier=='x')
+	  count+=print_digit(va_arg(ap,unsigned int),16);
+	else 
+	count+=write(1,&specifier,1);
+return count;
+
+}
+
 
 int	rohit_printf(const char *format, ...)
 {
@@ -23,3 +40,6 @@ int	rohit_printf(const char *format, ...)
 	va_end(ap);
 	return count;
 }
+
+
+ 

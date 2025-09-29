0155b558278fe94dd6a2cb1bc8a582b3d3374bdd
Rohit Patwa <therohitpatwa@gmail.com>
2025-09-29 21:21:55 +0530

    printf_format  implementation

--- myprintf.h
+++ myprintf.h
@@ -1,8 +1,22 @@
 #include<stdarg.h>
 
 
+int print_format(char specifier,va_list ap)
+{
+    int count=0;
+    if(specifier=='c')
+      count=print_char(va_arg(ap, int));
+    else if(specifier=='s')
+      count=print_str(va_arg(ap, char *));
+    else if(specifier=='d')
+       count=print_digit((long)va_arg(ap, int), 10);
+    else if(specifier=='x')
+       count=print_digit((long)va_arg(ap,unsigned int), 16);
+    else
+        count += write(1, &specifier, 1);
 
-
+        return count;
+}
 
 
 

90f62afb9a9c45ba4eac2ed814429e4142f76ff9
Rohit Patwa <therohitpatwa@gmail.com>
2025-09-29 21:28:27 +0530

    printf_digit  implementation

--- myprintf.h
+++ myprintf.h
@@ -1,5 +1,7 @@
 #include<stdarg.h>
-
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 
 
@@ -19,6 +21,26 @@ int print_str(char *s)
     return count;
 }
 
+
+int print_digit(int n,int base)
+{
+     int		count;
+	char	*symbols;
+
+	symbols = "0123456789abcdef";
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		return print_digit(-n, base) + 1;
+	}
+	else if (n < base)
+		return print_char(symbols[n]);
+	else
+	{
+		count = print_digit(n / base, base);
+		return count + print_digit(n % base, base);
+	}
+}
 
 
 int print_format(char specifier,va_list ap)
